7c0cae639982e546ef98ce71f0f108b9e14bd533
Skandakov <egor.skandakov@gmail.com>
2023-10-27 02:58:07 +0300

    fixed question table migration

--- db/migrations/000004_create_question_table.up.sql
+++ db/migrations/000004_create_question_table.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE nofronts.question (
     id BIGSERIAL PRIMARY KEY,
     form_id BIGINT NOT NULL REFERENCES nofronts.form(id),
-    question_type INTEGER,
+    question_type INTEGER NOT NULL,
     question_title VARCHAR NOT NULL,
     question_text TEXT NOT NULL DEFAULT '',
-    shuffle BOOLEAN DEFAULT FALSE
+    shuffle BOOLEAN NOT NULL DEFAULT FALSE
 );

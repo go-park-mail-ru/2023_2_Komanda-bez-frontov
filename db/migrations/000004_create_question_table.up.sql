97735adfa5ffe270b585c26b8d4da1b32bcbdc63
Skandakov <egor.skandakov@gmail.com>
2023-10-29 21:03:56 +0300

    added check constraint, removed default ''

--- db/migrations/000004_create_question_table.up.sql
+++ db/migrations/000004_create_question_table.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE nofronts.question (
     id BIGSERIAL PRIMARY KEY,
     form_id BIGINT NOT NULL REFERENCES nofronts.form(id),
-    question_type INTEGER NOT NULL,
-    question_title VARCHAR NOT NULL,
-    question_text TEXT NOT NULL DEFAULT '',
+    type INTEGER NOT NULL CHECK (type IN (1, 2, 3)),
+    title VARCHAR NOT NULL,
+    text TEXT NOT NULL,
     shuffle BOOLEAN NOT NULL DEFAULT FALSE
 );

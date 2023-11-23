cdb82e4bbb0132ca757d451fc9c2ecf3b953ee08
Spirin Mihail <misha_spirin_90@mail.ru>
2023-11-24 02:06:00 +0300

    delete constraints

--- db/migrations/000019_add_answer_constraint.sql
+++
@@ -1,3 +0,0 @@
-alter table passage_answer
-add constraint chk_PassageAnswer
-check (valid_passage_answer(question_id, answer_text) = true);
--- db/migrations/000021_add_anon_constraint.sql
+++
@@ -1,3 +0,0 @@
-alter table passage_answer
-add constraint chk_PassageAnswerAnon
-check (valid_anon_passage(question_id, user_id) = true);

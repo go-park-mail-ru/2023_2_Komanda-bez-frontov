6411a845169420694fd9d4507ae25ee19dc97134
Spirin Mihail <misha_spirin_90@mail.ru>
2023-11-26 16:07:12 +0300

    add cascades

--- db/migrations/000006_create_form_passage_table.up.sql
+++ db/migrations/000006_create_form_passage_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE nofronts.form_passage (
     id BIGSERIAL PRIMARY KEY,
     user_id BIGINT REFERENCES nofronts.user(id),
-    form_id BIGINT NOT NULL REFERENCES nofronts.form(id),
+    form_id BIGINT NOT NULL REFERENCES nofronts.form(id) ON DELETE CASCADE,
     finished_at TIMESTAMP NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC')
 );

5ebd6f80ccd1d78ef3a2d152936feae2cc6340a0
Kirill <kirill.l.2004@yandex.ru>
2023-12-25 14:36:11 +0300

    insert working

--- internal/repository/shortener_repository.go
+++ internal/repository/shortener_repository.go
@@ -31,62 +31,42 @@ func NewDatabaseRepository(db database.ConnPool, builder squirrel.StatementBuild
 }
 
 func (r *databaseRepository) Insert(ctx context.Context, url *URLMapping) (string, error) {
-    shortURL, err := generateRandomString(8)
-    if err != nil {
-        return "", err
-    }
+	shortURL, err := generateRandomString(8)
+	if err != nil {
+		return "", err
+	}
 
-    for {
-        exists, err := r.shortURLExists(ctx, shortURL)
-        if err != nil {
-            return "", fmt.Errorf("shortener_repository failed to check short URL existence: %v", err)
-        }
-        if !exists {
-            break
-        }
-        shortURL, err = generateRandomString(8)
-        if err != nil {
-            return "", err
-        }
-    }
+	tx, err := r.db.Begin(ctx)
+	if err != nil {
+		return "", fmt.Errorf("shortener_repository insert failed to begin transaction: %e", err)
+	}
 
-    query, args, err := r.builder.Insert(fmt.Sprintf("%s.url", r.db.GetSchema())).
-        Columns("long_url", "short_url").
-        Values(url.LongURL, shortURL).
-        Suffix("ON CONFLICT DO NOTHING").
-        ToSql()
-    if err != nil {
-        return "", fmt.Errorf("shortener_repository save_short_url failed to build query: %v", err)
-    }
+	defer func() {
+		switch err {
+		case nil:
+			err = tx.Commit(ctx)
+		default:
+			_ = tx.Rollback(ctx)
+		}
+	}()
 
-    tx, err := r.db.Begin(ctx)
-    if err != nil {
-        return "", fmt.Errorf("shortener_repository insert failed to begin transaction: %e", err)
-    }
+	query, args, err := r.builder.Insert(fmt.Sprintf("%s.url", r.db.GetSchema())).
+		Columns("long_url", "short_url").
+		Values(url.LongURL, shortURL).
+		Suffix("ON CONFLICT DO NOTHING").
+		ToSql()
+	if err != nil {
+		return "", fmt.Errorf("shortener_repository save_short_url failed to build query: %v", err)
+	}
 
-    defer func() {
-        switch err {
-        case nil:
-            err = tx.Commit(ctx)
-        default:
-            _ = tx.Rollback(ctx)
-        }
-    }()
+	_, err = tx.Exec(ctx, query, args...)
+	if err != nil {
+		return "", fmt.Errorf("shortener_repository insert failed to execute query: %e", err)
+	}
 
-    row := tx.QueryRow(ctx, query, args...)
-    if row == nil {
-        err = fmt.Errorf("shortener_repository insert failed to execute query: %e", err)
-        return "", err
-    }
-
-    keyURL := url.LongURL
-    err = row.Scan(&keyURL)
-    if err != nil {
-        return "", fmt.Errorf("shortener_repository insert failed to return key_url: %e", err)
-    }
-
-    return keyURL, nil
+	return shortURL, nil
 }
+
 
 func generateRandomString(length int) (string, error) {
     bytes := make([]byte, length)
@@ -100,42 +80,6 @@ func generateRandomString(length int) (string, error) {
     return randomString[:length], nil
 }
 
-func (r *databaseRepository) shortURLExists(ctx context.Context, shortURL string) (bool, error) {
-    existsQuery := squirrel.Select("EXISTS(SELECT 1)").
-        From(fmt.Sprintf("%s.url", r.db.GetSchema())).
-        Where(squirrel.Eq{"short_url": shortURL})
-
-    query, args, err := existsQuery.ToSql()
-    if err != nil {
-        return false, err
-    }
-
-    var exists bool
-    tx, err := r.db.Begin(ctx)
-    if err != nil {
-        return false, fmt.Errorf("shortener_repository shortURLExists failed to begin transaction: %v", err)
-    }
-
-    defer func() {
-        switch err {
-        case nil:
-            err = tx.Commit(ctx)
-        default:
-            _ = tx.Rollback(ctx)
-        }
-    }()
-
-    row := tx.QueryRow(ctx, query, args...)
-    if row == nil {
-        return false, fmt.Errorf("shortener_repository shortURLExists failed to execute query")
-    }
-
-    if err := row.Scan(&exists); err != nil {
-        return false, fmt.Errorf("shortener_repository shortURLExists failed to return exists value: %v", err)
-    }
-
-    return exists, nil
-}
 
 func (r *databaseRepository) RedirectHandler(w http.ResponseWriter, req *http.Request) {
 	shortURL := req.URL.Path[len("/redirect/"):]
